a6c39ea6790585b1f48ac03dc2a61265216bb174
David Groomes <dwgroomes@gmail.com>
2021-01-02 13:33:33 -0600

    Run the JShell tool in debug mode

--- with-libraries/run-jshell.sh
+++ with-libraries/run-jshell.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 # Start a jshell session that's loaded with the project's source code and library dependencies.
 # NOTE: JShell is awesome.
+#
+# Optionally pass the option "--debug" and the Java process running the JShell tool itself will start in the debugging
+# mode and will be suspended until a remote debugger is attached.
 
-jshell \
-  --feedback normal \
-  --startup DEFAULT --startup setup-snippets.jsh \
-  --class-path 'out:lib/*' \
-  --enable-preview
+set -e
+
+opts=(
+--feedback normal
+--startup DEFAULT --startup setup-snippets.jsh
+--class-path 'out:lib/*'
+--enable-preview
+)
+
+if [[ "x$1" == "x--debug" ]]; then
+  echo "'debug' flag detected. JShell will be started in debug mode"
+  opts+=('-J-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005')
+fi
+
+set -u
+
+jshell "${opts[@]}"
